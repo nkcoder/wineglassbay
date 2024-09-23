90be9294495d6487085f07a482001813f39e50a3
daniel.guo <daniel.guo@vivalabs.com.au>
2024-09-24 08:55:07 +1000

    Complete 58_length_of_last_word

--- 58_length_of_last_word.scala
+++ 58_length_of_last_word.scala
@@ -1,9 +1,9 @@
 // https://leetcode.com/problems/length-of-last-word
 
-/**
-  * Run: scala-cli run 58_length_of_last_word.scala 
+/** Run: scala-cli run 58_length_of_last_word.scala
   */
 object Solution {
+  // scan from the beginning
   def lengthOfLastWord(s: String): Int = {
     var i = 0
     var count = 0
@@ -27,9 +27,38 @@ object Solution {
     return count;
   }
 
+  // scan from the end
+  def lengthOfLastWord2(s: String): Int = {
+    val n = s.length();
+    var i = n - 1;
+    var count = 0;
+    while (i >= 0 && s(i) == ' ') {
+      i -= 1;
+    }
+
+    while (i >= 0 && s(i) != ' ') {
+      count += 1;
+      i -= 1;
+    }
+
+    return count;
+  }
+
+  def lengthOfLastWord3(s: String): Int = {
+    s.trim.split(" ").last.length
+  }
+
+  def lengthOfLastWord4(s: String): Int = {
+    s.trim.split(" ").reverse.head.length
+  }
+
   def main(args: Array[String]): Unit = {
     assert(lengthOfLastWord("Hello World") == 5)
     assert(lengthOfLastWord("   fly me   to   the moon  ") == 4)
     assert(lengthOfLastWord("luffy is still joyboy") == 6)
+
+    assert(lengthOfLastWord2("Hello World") == 5)
+    assert(lengthOfLastWord2("   fly me   to   the moon  ") == 4)
+    assert(lengthOfLastWord2("luffy is still joyboy") == 6)
   }
 }
